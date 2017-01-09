6953dbadbf9be6c138a34f76df80061baf65bd96
Tranquility <tranquilitydev@outlook.com>
2017-01-09 23:10:49 +0800

    Add files via upload

---
+++ form/post.js
@@ -0,0 +1,27 @@
+﻿var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/www/index.html')
+});
+
+app.use(bodyParser.json());
+
+app.post('/temp',function(req, res){
+	var json = req.body;
+	console.log("溫度：" + json.temp);   // 在控制台顯示溫度值
+	console.log("濕度：" + json.humid);
+	}
+);
+
+app.get('*', function(req, res) {
+  res.status(404);
+  res.send('找不到網頁！');
+});
+
+app.listen(1234, function(req, res) {
+  console.log("網站伺服器在1234埠口開工了！");
+});
